97e44517c55aed5eca4ea851ae4acca2a8c648ed
psoukie <42703196+psoukie@users.noreply.github.com>
2022-01-19 14:04:14 -0800

    fixing sidenotes

--- assets/js/sidenotes.js
+++ assets/js/sidenotes.js
@@ -176,7 +176,7 @@ function ridiculousWorkaroundsForBrowsersFromBizarroWorld() {
                     }
                 }
                 @media only screen and (max-width: ${viewportWidthBreakpointInPixels}px) {
-                    .footnote-ref:target {
+                    .footnote:target {
                         background-color: inherit;
                         box-shadow: none;
                     }
@@ -720,7 +720,7 @@ function constructSidenotes() {
         */
     GW.sidenotes.sidenoteDivs = [ ];
     //  The footnote references (citations).
-    GW.sidenotes.footnoteRefs = Array.from(document.querySelectorAll("a.footnote-ref"));
+    GW.sidenotes.footnoteRefs = Array.from(document.querySelectorAll("a.footnote"));
     for (var i = 0; i < GW.sidenotes.footnoteRefs.length; i++) {
         //  Create the sidenote outer containing block...
         let sidenote = document.createElement("div");
